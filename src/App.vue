1ce1e7f16804ecb3a38424007a89dc413c3a2664
olegvyazovoy <oleg.vyazovoy@nextiva.com>
2018-08-13 18:31:14 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title">
+      <h1>
+        {{ episod_name }}
+        <span>{{episode_id}}</span>
+      </h1>
+      <span class="release_date">date</span>
+    </div>
+    <button @click="add">add</button>
+    <input v-model="episod_name" type="text">
+      
+    <div id="moveInfo">
+      
+      <p class="director">director</p>
+      <p class="producer">producer</p>
+    </div>
+    <ul id="charactersList">
+      <li>one</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      episod_name: "Some episode name",
+      episode_id: 1
+    };
+  },
+  methods: {
+      changeEpisodeName(event){
+        this.episod_name = event.target.value;
+      },
+      add(){
+        this.episode_id += 1;
+      }
+  },
 }
 </script>
 
@@ -21,8 +47,22 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 50%;
+  max-width: 50%;
+  margin: 0 auto;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: antiquewhite;
+}
+.title {
+  grid-column-start: 1;
+  grid-column-end: 3;
+}
+
+#charactersList{
+  background-color: aquamarine
 }
 </style>
